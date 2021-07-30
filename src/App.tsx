55c866de101a25f074241b3700a03debd639bf21
Hanasagi <xenergymods@gmail.com>
2021-07-30 18:20:44 +0200

    fixed cors issue & tried using sqlite

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { Redirect, Route } from 'react-router-dom';
+import {
+  Redirect,
+  Route
+} from 'react-router-dom';
 import {
   IonApp,
   IonIcon,
@@ -8,9 +11,22 @@ import {
   IonTabButton,
   IonTabs,
 } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {
+  IonReactRouter
+} from '@ionic/react-router';
+import {
+  ellipse,
+  square,
+  triangle
+} from 'ionicons/icons';
+import {
+  RouteComponentProps
+} from 'react-router';
 import Home from './components/Home'
+import React from 'react'
+import cheerio from 'cheerio';
+import { HTTP } from '@ionic-native/http';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -31,35 +47,262 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+
+interface State {
+  loading: boolean,
+    alldata: Array < any > ,
+    data: {
+      title: string,
+      volume: string,
+      thumbnail: string,
+      ISBN: string
+    }
+}
+
+class App extends React.Component < any, State > {
+    constructor(props: any) {
+      super(props);
+
+      this.state = {
+        loading: false,
+        alldata: [],
+        data: {
+          title: "",
+          volume: "",
+          thumbnail: "",
+          ISBN: ""
+        }
+      }
+      this.handleChange = this.handleChange.bind(this)
+      this.getLists = this.getLists.bind(this)
+      this.createList = this.createList.bind(this)
+      this.getList = this.getList.bind(this)
+      this.updateList = this.updateList.bind(this)
+      this.deleteList = this.deleteList.bind(this)
+      this.changeDataState = this.changeDataState.bind(this)
+    }
+    componentDidMount(){
+      SQLiteObject.executeSql('SHOW Tables',[]).then((r) => console.log(r))
+      SQLite.create({
+        name: 'data.db',
+        location: 'default'
+      })
+      .then((db: SQLiteObject) => {
+
+
+    db.executeSql('create table danceMoves(name VARCHAR(32))', [])
+      .then(() => console.log('Executed SQL'))
+      .catch(e => console.log(e));
+    })
+  .catch(e => console.log(e));
+    }
+
+
+    getLists() {
+      this.setState({
+        loading: true
+      }, () => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+          .then(res => res.json())
+          .then(result =>
+            this.setState({
+              loading: false,
+              alldata: result
+            })
+          )
+          .catch(console.log);
+      });
+    }
+
+    handleSubmit(e: any) {
+      e.preventDefault();
+    }
+
+    handleChange(e: any) {
+      const {
+        name,
+        value
+      } = e.target;
+      this.setState({
+        data: {
+          ...this.state.data,
+          [name]: value
+        }
+      });
+      console.log(this.state.data)
+    }
+    changeDataState(r: any) {
+                console.log(r.items[0],r.items[0].volumeInfo.title)
+      let title = r.items[0].volumeInfo.title;
+      title=title.substring(0, title.indexOf("Tome") - 1)
+      let volume = r.items[0].volumeInfo.title;
+      volume=volume.substring(volume.length - 1)
+      let ISBN = r.items[0].volumeInfo.industryIdentifiers[1];
+      let thumbnail = this.scrapeMangaCover(title, volume);
+
+    }
+    scrapeMangaCover(title: string, vol: string) {
+      let transformTitle1 = (title: string) => {
+        let match = title.match(/^l['ea]s?/gm) !;
+        title = title.substring(match[0].length, title.length).trim();
+        title = title + "-" + match;
+        return title;
+      }
+
+      let transformTitle2 = (title: string) => {
+        title = title.replaceAll(' ', "-");
+        if (title.match(/[À-ÖØ-öø-ÿ]/gmu)) title = title.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+
+        let regex = /[^\w\s^-]/gmu;
+        if (title.match(regex)) title = title.replace(regex, "");
+
+        regex = /---/gm;
+        if (title.match(regex)) title = title.replace(regex, "-");
+
+        title = title.charAt(0).toUpperCase() + title.slice(1);
+        return title;
+      }
+
+      if (title.match(/^l['ea]s?/gm)) {
+        let match = title.match(/^l['ea]s?/gm);
+        title = transformTitle1(title);
+      }
+      title = transformTitle2(title);
+      console.log("https://www.manga-news.com/index.php/manga/" + title + "/vol-" + vol)
+      HTTP.get("https://www.manga-news.com/index.php/manga/" + title + "/vol-" + vol,{},{}).then((r) => {
+        console.log(r.data)
+        return r.data;
+      }).then(r=>{
+        console.log(r)
+        const $ = cheerio.load(r)
+        console.log($("#picinfo > a > img").attr('src'))
+      }).catch(e => console.log(e.message));
+    }
+    createList() {
+      fetch("http://localhost:3000/manga", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.state.data)
+      }).then(r =>
+        this.setState({
+          data: {
+            title: "",
+            volume: "",
+            thumbnail: "",
+            ISBN: ""
+          }
+        })
+      );
+    }
+
+    getList(event: any, id: number) {
+      this.setState({
+          data: {
+            title: "Loading",
+            volume: "Loading",
+            thumbnail: "Loading",
+            ISBN: "Loading"
+          }
+        },
+        () => {
+          fetch("https://jsonplaceholder.typicode.com/posts/" + id)
+            .then(res => res.json())
+            .then(result => {
+              this.setState({
+                data: {
+                  title: result.title,
+                  volume: result.volume,
+                  thumbnail: result.thumbnail,
+                  ISBN: result.ISBN
+                }
+              });
+            });
+        }
+      );
+    }
+
+    updateList(event: any, id: number) {
+      fetch("https://jsonplaceholder.typicode.com/posts/" + id, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(this.state.data)
+        })
+        .then(res => res.json())
+        .then(result => {
+          this.setState({
+            data: {
+              title: "",
+              volume: "",
+              thumbnail: "",
+              ISBN: ""
+            }
+          });
+          this.getLists();
+        });
+    }
+
+    deleteList(event: any, id: number) {
+      fetch("https://jsonplaceholder.typicode.com/posts/" + id, {
+          method: "DELETE"
+        })
+        .then(res => res.json())
+        .then(result => {
+          this.setState({
+            data: {
+              title: "",
+              volume: "",
+              thumbnail: "",
+              ISBN: ""
+            }
+          });
+          this.getLists();
+        });
+    }
+  render(){
+    const {data,alldata,loading}=this.state
+    return(
+    <IonApp>
+        <IonReactRouter>
+          <IonTabs>
+            <IonRouterOutlet>
+              <Route exact path="/home">
+                <Home alldata={this.state.alldata}
+                              data={this.state.data}
+                              get={this.getList}
+                              update={this.updateList}
+                              delete={this.deleteList}
+                              create={this.createList}
+                              handleChange={this.handleChange}
+                              changeDataState={this.changeDataState}/>
+              </Route>
+              <Route exact path="/">
+                <Redirect to="/home" />
+              </Route>
+            </IonRouterOutlet>
+            <IonTabBar slot="bottom">
+              <IonTabButton tab="tab1" href="/tab1">
+                <IonIcon icon={triangle} />
+                <IonLabel></IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="tab2" href="/tab2">
+                <IonIcon icon={ellipse} />
+                <IonLabel>Tab 2</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="tab3" href="/tab3">
+                <IonIcon icon={square} />
+                <IonLabel>Tab 3</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        </IonReactRouter>
+      </IonApp>
+    )
+   }
+
+}
 
 export default App;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,61 +1,101 @@
-import { 
-  IonContent, 
-  IonInput, 
-  IonButton, 
-  IonItem, 
-  IonHeader, 
-  IonPage, 
-  IonTitle, 
-  IonToolbar } from '@ionic/react';
+import {
+  IonContent,
+  IonInput,
+  IonButton,
+  IonItem,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from '@ionic/react';
 import React from 'react';
+import {
+  RouteProps
+} from 'react-router';
 
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import {
+  BarcodeScanner
+} from '@ionic-native/barcode-scanner';
+import { HTTP } from '@ionic-native/http';
 
-class Home extends React.Component {
+interface Props {
+  alldata: any,
+  data: any
+  get: any
+  update: any
+  delete: any
+  handleChange: any,
+  create: any,
+  changeDataState:any
+}
+
+class Home extends React.Component < Props > {
+
+  constructor(props: any) {
+    super(props)
+    console.log(props)
+  }
 
   state = {
     stringEncoded: '',
-    encodeResponse: 'Hello World',
     dataEncode: ''
   }
-
-  handleChange = (e: any) => {
-    const { value, name } = e.target;
-    this.setState({ 
-      [name]: value }
-    );
-    console.log(this.state);
-  };
 
   render() {
 
     const dataToScan = async () => {
       const data = await BarcodeScanner.scan();
-      alert(JSON.stringify(data));
-      this.setState({ stringEncoded: data.text })
-      fetch("https://openlibrary.org/api/books?bibkeys=ISBN:"+this.state.stringEncoded+"&jscmd=details&format=json").then((r)=>{
-                               return r.json();
-                          }).then(r=>alert(JSON.stringify(r))).catch(function(error) {
-                               alert('Il y a eu un problème avec l\'opération fetch: ' + error.message);
-                             });
+      this.setState({
+        stringEncoded: data.text
+      })
+      fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:" + this.state.stringEncoded + "&key=AIzaSyCoxS6O4YfwkNs87X59KRcoEOxZWU7Xlpg")
+        .then((r) => {
+          return r.json();
+        })
+        .then(r => {this.props.changeDataState(r)})
+        .catch(function(error) {
+          console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+        });
     };
 
+    let handleSubmit = async (e:any) => {
+    e.preventDefault();
+    console.log()
+    HTTP.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + this.props.data.ISBN + "&key=AIzaSyCoxS6O4YfwkNs87X59KRcoEOxZWU7Xlpg",{},{})
+            .then(r => {
+              return r.data;
+            })
+            .then(r => {
+
+            this.props.changeDataState(r)
+          })
+           .catch(function(error) {
+              console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+            });
+    }
+
     return (
-      <IonPage>
+		 <IonPage>
         <IonHeader>
           <IonToolbar>
             <IonTitle>Ionic QR/Barcode Scanner Example</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent className="ion-padding">
-
-          <strong>Scan Content</strong>
-
-          <IonButton color="danger" expand="block" onClick={dataToScan}>
-              Scan Data 
+        <IonButton onClick={dataToScan}>
+          <strong>AAAAAAA</strong>
           </IonButton>
-
         </IonContent>
+        <form onSubmit={handleSubmit}>
+            <IonInput name="title" placeholder="title" onIonChange={this.props.handleChange}></IonInput>
+            <IonInput name="volume"  placeholder="volume" onIonChange={this.props.handleChange}></IonInput>
+            <IonInput name="thumbnail" placeholder="thumbnail" onIonChange={this.props.handleChange}></IonInput>
+            <IonInput name="ISBN" placeholder="ISBN" onIonChange={this.props.handleChange}></IonInput>
+            <input type="submit"/>
+        </form>
       </IonPage >
     );
   }
